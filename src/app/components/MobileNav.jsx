0011655ce8b41caf7acdac2ce09f405fc7d07947
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2024-09-02 08:10:22 +0200

    Update MobileNav.jsx

--- src/app/components/MobileNav.jsx
+++ src/app/components/MobileNav.jsx
@@ -1,36 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import LinkedinIcon from "../svg/LinkedinIcon";
+import Modal from "../ui/Modal";
 
 export const MobileNav = ({ closeMenuBtn }) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
   return (
     <nav className="fixed py-[40px] top-0 right-0 bg-[#071468] w-[80%] h-screen z-50">
       <div className="container">
-      <div className="container flex justify-end">
-        <button type="button" onClick={closeMenuBtn} className="inline-block">
-          <Image src="/close.svg" width={24} height={24} alt="close" />
-        </button>
-      </div>
+        <div className="container flex justify-end">
+          <button type="button" onClick={closeMenuBtn} className="inline-block">
+            <Image src="/close.svg" width={24} height={24} alt="close" />
+          </button>
+        </div>
         <ul className="mt-[48px] lg:mt-0 flex flex-col gap-[32px] lg:flex-row items-center lg:gap-[16px] text-lg text-mainWhite">
           <li>
-            <a href="#about " onClick={closeMenuBtn}>About me</a>
-          </li>
-          <li> 
-            <a href="#services" onClick={closeMenuBtn}>Services</a>
+            <a href="#about " onClick={closeMenuBtn}>
+              About me
+            </a>
           </li>
           <li>
-            <a href="#history" onClick={closeMenuBtn}>Reviews</a>
+            <a href="#services" onClick={closeMenuBtn}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#history" onClick={closeMenuBtn}>
+              Reviews
+            </a>
           </li>
         </ul>
-        <div className="mt-[48px] lg:mt-0 text-base text-mainWhite">
-          <p className="mb-[12px] text-center lg:text-left">
-            Have any questions?
-          </p>
-          <a href="mailto:tim@brotherinblue.org" type="email" className="flex justify-center">
-            <Image src="/sms-tracking.svg" width={24} height={24} alt="email" />
-            <span className="ml-[6px]">tim@brotherinblue.org</span>
+        <div className="mt-[48px] flex flex-col-reverse items-center z-10">
+          <button
+            type="button"
+            onClick={handleOpenModal}
+            className="mt-[16px] p-3 border rounded-[8px] font-bold hover:bg-mainWhite text-mainWhite hover:text-mainBlue ease-in duration-200"
+          >
+            Keep in Contact
+          </button>
+          <a
+            href="https://linkedin.com"
+            target="_black"
+            rel="noopener noreferrer nofollow"
+          >
+            <LinkedinIcon />
           </a>
         </div>
       </div>
+      {openModal && <Modal setOpenModal={setOpenModal} />}
     </nav>
   );
 };
